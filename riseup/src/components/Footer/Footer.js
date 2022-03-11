5372559ee2991e3624de0fa4b1871060e9fcb95d
heather loop <hloop132@gmail.com>
2022-03-10 22:54:31 -0800

    nile

--- riseup/src/components/Footer/Footer.js
+++ riseup/src/components/Footer/Footer.js
@@ -1,30 +1,34 @@
 import React from "react";
-import "../Footer/footer.css"
+import "../Footer/footer.css";
 
 function Footer() {
   return (
     <div className="main-footer">
       <div className="containerFooter">
         <div className="column">
-
-        <div className="row">
-          {/* Column1 */}
-    
-          <div className="col">
-            <h4 className="list-unstyled">
-            🌻Created by: Chrissie, Denise, Heather, Nile🌻
-            </h4>
-          </div>
-          <hr />
           <div className="row">
-            <p className="col-sm">
-              &copy;{new Date().getFullYear()} Rise Up | All rights reserved |
-              <a href="#" className="footTag">Terms Of Service </a>| <a href="#" className="footTag">Privacy</a>
-            </p>
+            {/* Column1 */}
+
+            <div className="col">
+              <h4 className="list-unstyled">
+                🌻Created by: Chrissie, Denise, Heather, Nile🌻
+              </h4>
+            </div>
+            <hr />
+            <div className="row">
+              <p className="col-sm">
+                &copy;{new Date().getFullYear()} Rise Up | All rights reserved |
+                <a href="#" className="footTag">
+                  Terms Of Service
+                </a>
+                <a href="#" className="footTag">
+                  Privacy
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
