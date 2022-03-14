d2ddfeb793597b0b9c5a2a50b45b5978bf2cf120
heather loop <hloop132@gmail.com>
2022-03-14 15:50:20 -0700

    work

--- riseup/src/components/Navbar/Navbar.js
+++ riseup/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import useAuth from "../../utils/hooks/useAuth";
-import API from '../../utils/API';
+import API from "../../utils/API";
 import "../Navbar/navbar.css";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
@@ -10,23 +10,21 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useMediaQuery } from "@mui/material";
 
 const options = [
-  {name:"Home", href:"/"},
-  {name:"About", href:"/about"},
-  {name:"Horoscope", href:"/horoscope"},
-  {name:"Crisis Links", href:"/crisis"},
-  {name:"Login",href:"/users/login"},
-  {name:"Sign Up", href:"/signup"},
-  {name:"Logout",href:"/logout"}
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Horoscope", href: "/horoscope" },
+  { name: "Crisis Links", href: "/crisis" },
+  { name: "Login", href: "/users/login" },
+  { name: "Sign Up", href: "/signup" },
+  { name: "Logout", href: "/logout" },
 ];
 
 const ITEM_HEIGHT = 48;
 
 const Navbar = (props) => {
-  const isBigScreen = useMediaQuery("(min-width:913px)")
-  const [navUID, setNavUID] = useState("");
-  useEffect(() => {
-    setNavUID(props.id);
-  });
+  const isBigScreen = useMediaQuery("(min-width:913px)");
+  const navigate = useNavigate();
+  const { auth, setAuth } = useAuth();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -36,54 +34,45 @@ const Navbar = (props) => {
     setAnchorEl(null);
   };
 
-
-const Navbar = (props) => {
-  const navigate = useNavigate();
-  const {auth, setAuth} = useAuth();
-  // const [navUID, setNavUID] = useState('')
-  // useEffect(()=>{
-  //   setNavUID(props.id);
-  // })
-
-const redirect = (e, url) => {
-  e.preventDefault();
+  const redirect = (e, url) => {
+    e.preventDefault();
     const token = localStorage.getItem("token");
     if (token) {
-
-      API.getTokenData(localStorage.getItem('token'))
-        .then(data => {
-    
-          console.log('data from token check', data);
+      API.getTokenData(localStorage.getItem("token"))
+        .then((data) => {
+          console.log("data from token check", data);
           setAuth({
-            userId: data.userId, 
-            userName: data.username, 
-            role: data.role, 
+            userId: data.userId,
+            userName: data.username,
+            role: data.role,
+          });
         })
-      
-        }).then(newdata=>{
+        .then((newdata) => {
           navigate(url);
         })
-        
-        .catch(err => {
+
+        .catch((err) => {
           console.log(err);
         });
+    } else {
+      console.log("no token");
+    }
+  };
 
-    } else {console.log('no token')}
-  }
+  console.log("this is auth", auth);
 
-  console.log('this is auth', auth)
-
-  const logMeOut = ()=>{
+  const logMeOut = () => {
     localStorage.removeItem("token");
     setAuth({
-      userId: '', 
-      userName: '', 
-      role: '', 
-      token: ''})
-    console.log('you\'re logged out!')
-    navigate('/')
-  }
- 
+      userId: "",
+      userName: "",
+      role: "",
+      token: "",
+    });
+    console.log("you're logged out!");
+    navigate("/");
+  };
+
   return (
     <nav>
       <div className="containerNav">
@@ -92,84 +81,79 @@ const redirect = (e, url) => {
           <input className="inputNav" type="text" />
         </div>
         <div className="menuNav">
-      {!auth.userName ? 
-          <>
-            <a className="navA is-active" href='/'>
-              Home
+          {!auth.userName ? (
+            <>
+              <a className="navA is-active" href="/">
+                Home
+              </a>
+              <a className="navA" href="/login">
+                Login
+              </a>
+              <a className="navA" href="/signup">
+                Sign Up
+              </a>
+            </>
+          ) : (
+            <a className="navA" href="/profile">
+              User Home
             </a>
-            <a className="navA" href="/login">
-              Login
-            </a>
-            <a className="navA" href="/signup">
-              Sign Up
-            </a>
-          </>
-        : 
-          <a className="navA" href="/profile">
-            User Home
-          </a> 
-          }
-          <a className="navA" href='/about'>
+          )}
+          <a className="navA" href="/about">
             About
           </a>
-          <a className="navA" href='/horoscope'>
+          <a className="navA" href="/horoscope">
             Horoscope
           </a>
-          <a className="navA" href='/crisis'>
+          <a className="navA" href="/crisis">
             Crisis Links
           </a>
           <a className="navA" onClick={logMeOut}>
             Logout
           </a>
         </div>
-    {!isBigScreen?(
-      <>
-        <IconButton
-          aria-label="more"
-          id="long-button"
-          aria-controls={open ? "long-menu" : undefined}
-          aria-expanded={open ? "true" : undefined}
-          aria-haspopup="true"
-          onClick={handleClick}
-        >
-          <MoreVertIcon />
-        </IconButton>
-        <Menu
-          id="long-menu"
-          MenuListProps={{
-            "aria-labelledby": "long-button",
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              display: "flex",
-              flexDirection: "column",
-              maxHeight: ITEM_HEIGHT * 4.5,
-            },
-            
-          }}
-        >
-          {options.map((option) => (
-            <MenuItem
-              key={option.name}
-              selected={option === "Pyxis"}
-              onClick={handleClose}
+        {!isBigScreen ? (
+          <>
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-controls={open ? "long-menu" : undefined}
+              aria-expanded={open ? "true" : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
             >
-              <a href={option.href}>
-                {option.name} 
-              </a>
-            </MenuItem>
-          ))}
-        </Menu>
-        </>
-    ):null}
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id="long-menu"
+              MenuListProps={{
+                "aria-labelledby": "long-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  display: "flex",
+                  flexDirection: "column",
+                  maxHeight: ITEM_HEIGHT * 4.5,
+                },
+              }}
+            >
+              {options.map((option) => (
+                <MenuItem
+                  key={option.name}
+                  selected={option === "Pyxis"}
+                  onClick={handleClose}
+                >
+                  <a href={option.href}>{option.name}</a>
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
+        ) : null}
       </div>
     </nav>
   );
 };
-}
+
 export default Navbar;
-
-
