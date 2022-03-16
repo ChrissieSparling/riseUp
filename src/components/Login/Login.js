4080932c665e958d515a23c820a8eab379988459
Buttercup <andyandnile@gmail.com>
2022-03-15 19:36:14 -0700

    added form protections in front

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,6 +12,8 @@ function Login() {
   const location = useLocation();
   const from = location.state?.from?.pathname || '/profile';
 
+  const [usernameShow, setUsernameShow] = useState('none');
+  const [passwordShow, setPasswordShow] = useState('none');
   const [username, setUsername] = useState("");
   const [loginInfo, setLoginInfo] = useState({
     username: '',
@@ -27,28 +29,42 @@ function Login() {
   }
 
   async function handleLogin(e) {
-    e.preventDefault()
-    console.log('login info', loginInfo)
-    API.login(loginInfo.username, loginInfo.password)
-      .then(data => {
-        console.log(data);
-        if (data.accessToken) {
-          const userId = data?.id;
-          const userName = data?.username
-          const token = data?.accessToken;
-          const role = data?.role;
-          setAuth({ userId, userName, role, token })
-          console.log('auth info', userId, userName, role, token)
-          localStorage.setItem("token", data.accessToken);
-          setLoginInfo({
-            username: '',
-            password: ''
-          })
-          navigate(from, { replace: true });
-        } else { alert('Your username or password was incorrect!') }
-      }).catch(err => {
-        console.log(err);
-      });
+    if(!loginInfo.username && !loginInfo.password){
+      setUsernameShow('inline')
+      setPasswordShow('inline')
+      return
+    }
+    if (!loginInfo.username) {
+      setUsernameShow('inline')
+      return
+    }
+    if (!loginInfo.password) {
+      setPasswordShow('inline')
+      return
+    }
+    if (loginInfo.username!=='' && loginInfo.password!=='') {
+      console.log('login info', loginInfo)
+      API.login(loginInfo.username, loginInfo.password)
+        .then(data => {
+          console.log(data);
+          if (data.accessToken) {
+            const userId = data?.id;
+            const userName = data?.username
+            const token = data?.accessToken;
+            const role = data?.role;
+            setAuth({ userId, userName, role, token })
+            console.log('auth info', userId, userName, role, token)
+            localStorage.setItem("token", data.accessToken);
+            setLoginInfo({
+              username: '',
+              password: ''
+            })
+            navigate(from, { replace: true });
+          } else { alert('Your username or password was incorrect!') }
+        }).catch(err => {
+          console.log(err);
+        });
+    }
   };
 
   return (
@@ -62,22 +78,30 @@ function Login() {
           <div>
             <h1 className="loginTitle">Login</h1>
             <div className="email-input">
+              <div className="form-alert-box">
                 <input type="text"
+                  required
                   placeholder="username"
                   onChange={handleLoginInputChange}
                   value={loginInfo.username}
                   name="username"
                   className="input-field inputLogin" />
+                <p className="form-alert" style={{ display: `${usernameShow}` }}>This field is required. Please provide a valid username.</p>
+              </div>
             </div>
             <div>
+              <div className="form-alert-box">
                 <input
                   type="password"
+                  required
                   onChange={handleLoginInputChange}
                   value={loginInfo.password}
                   placeholder="password"
                   name="password"
                   className="input-field inputLogin"
                 />
+                <p className="form-alert" style={{ display: `${passwordShow}` }}>This field is required. Please enter your password.</p>
+              </div>
             </div>
 
             <div className="login-btn">
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,87 +1,205 @@
 import { React, useState } from "react";
 import "../Login/login.css";
+import API from "../../utils/API"
+import useAuth from '../../utils/hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 
 function SignUp(props) {
+  const navigate = useNavigate();
+  const { auth, setAuth } = useAuth();
+  const [username, setUsername] = useState("");
+  const [usernameShow, setUsernameShow] = useState('none');
+  const [passwordShow, setPasswordShow] = useState('none');
+  const [firstNameShow, setFirstNameShow] = useState('none');
+  const [lastNameShow, setLastNameShow] = useState('none');
+  const [emailShow, setEmailShow] = useState('none');
+  const [birthdayShow, setBirthdayShow] = useState('none');
+  const [zipCodeShow, setZipCodeShow] = useState('none');
+  const [newUser, setNewUser] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    role: 'paidUser',
+    email: '',
+    birthday: '',
+    zipCode: '',
+  });
+  const [userId, setUserId] = useState(0);
+  const [token, setToken] = useState("");
+
+
+
+  const handlePostUser = (e) => {
+    console.log(newUser)
+    e.preventDefault();
+    if (!newUser.username) {
+      setUsernameShow('inline')
+      return
+    }
+    if (!newUser.firstName) {
+      setFirstNameShow('inline')
+      return
+    }
+    if (!newUser.lastName) {
+      setLastNameShow('inline')
+      return
+    }
+    if (!newUser.email) {
+      setEmailShow('inline')
+      return
+    }
+    if (!newUser.birthday) {
+      setBirthdayShow('inline')
+      return
+    }
+    if (!newUser.zipCode) {
+      setZipCodeShow('inline')
+      return
+    }
+    if (!newUser.password) {
+      setPasswordShow('inline')
+      return
+    }
+    // if (!newUser.password) {
+    //   setPasswordShow('inline')
+    //   return
+    // }
+    if (newUser.username !== '' && newUser.password !== '' && newUser.firstName !== '' && newUser.lastName !== '' && newUser.email !== '' && newUser.birthday !== '' && newUser.zipCode !== '') {
+      API.signUp(newUser)
+        .then((newData) => {
+          if (newData.id) {
+            console.log(newData)
+            setUserId(newData.id);
+            setUsername(newData.username);
+            setToken(newData.accessToken);
+            localStorage.setItem("token", newData.accessToken);
+            navigate(`/login`)
+          } else {
+            alert('Your request was not successful. \nPlease check the form and try again.');
+            console.log('front end post req prob:', newData)
+            return
+          }
+        })
+        .catch(err => {
+          alert('Sorry! Our bad, there was a problem.');
+          console.log('there was an error', err)
+          return
+        })
+    }
+  };
+
+  const handleCollectUser = e => {
+    console.log(e.target.name, e.target.value)
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value
+    })
+  }
+
+
+
   return (
+
     <div className="mainSign">
       <div className="sub-main-sign">
         <div>
           <div>
-            <form onSubmit={props.handlePostUser}>
+            <form onSubmit={handlePostUser}>
               <h1 className="signTitle">Sign Up</h1>
 
               <div className="email-input">
-                <input
-                  type="text"
-                  placeholder="first name"
-                  className="input-field inputSign"
-                  required
-                  name='firstName'
-                  onChange={props.handleCollectUser}
-                />
+                <div className="form-alert-box">
+                  <input
+                    type="text"
+                    placeholder="first name"
+                    className="input-field inputSign"
+                    required
+                    name='firstName'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${firstNameShow}` }}>This field is required. Please enter your first name.</p>
+                </div>
               </div>
               <div className="email-input">
-                <input
-                  type="text"
-                  placeholder="
-                last name"
-                  className="input-field inputSign"
-                  required
-                  name='lastName'
-                  onChange={props.handleCollectUser}
-                />
-              </div>
-
-              <div className="email-input">
-                <input
-                  type="date"
-                  placeholder="birthday"
-                  className="input-field inputSign"
-                  required
-                  name='birthday'
-                  onChange={props.handleCollectUser}
-                />
-              </div>
-              <div className="email-input">
-                <input
-                  type="text"
-                  placeholder="zip code"
-                  className="input-field inputSign"
-                  required
-                  name='zipCode'
-                  onChange={props.handleCollectUser}
-                />
+                <div className="form-alert-box">
+                  <input
+                    type="text"
+                    placeholder="last name"
+                    className="input-field inputSign"
+                    required
+                    name='lastName'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${lastNameShow}` }}>This field is required. Please enter your last name.</p>
+                </div>
               </div>
 
               <div className="email-input">
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="input-field inputSign"
-                  required
-                  name='email'
-                  onChange={props.handleCollectUser}
-                />
+                <div className="form-alert-box">
+                  <input
+                    type="date"
+                    placeholder="birthday"
+                    className="input-field inputSign"
+                    required
+                    name='birthday'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${birthdayShow}` }}>This field is required. Please enter your birthday.</p>
+                </div>
               </div>
               <div className="email-input">
-                <input
-                  type="text"
-                  placeholder="user name"
-                  className="input-field inputSign"
-                  required
-                  name='username'
-                  onChange={props.handleCollectUser}
-                />
+                <div className="form-alert-box">
+                  <input
+                    type="text"
+                    placeholder="zip code"
+                    className="input-field inputSign"
+                    required
+                    name='zipCode'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${zipCodeShow}` }}>This field is required. Please enter your zip code.</p>
+                </div>
+              </div>
+
+              <div className="email-input">
+                <div className="form-alert-box">
+                  <input
+                    type="text"
+                    placeholder="email"
+                    className="input-field inputSign"
+                    required
+                    name='email'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${emailShow}` }}>This field is required. Please enter your email.</p>
+                </div>
               </div>
               <div className="email-input">
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input-field inputSign"
-                  required
-                  name='password'
-                  onChange={props.handleCollectUser}
-                />
+                <div className="form-alert-box">
+                  <input
+                    type="text"
+                    placeholder="user name"
+                    className="input-field inputSign"
+                    required
+                    name='username'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${usernameShow}` }}>This field is required. Please pick a username.</p>
+                </div>
+              </div>
+              <div className="email-input">
+                <div className="form-alert-box">
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input-field inputSign"
+                    required
+                    name='password'
+                    onChange={handleCollectUser}
+                  />
+                  <p className="form-alert" style={{ display: `${passwordShow}` }}>This field is required. Please choose a password.</p>
+                </div>
               </div>
               {/* <div className="email-input">
                 <input
@@ -94,9 +212,13 @@ function SignUp(props) {
                 />
               </div> */}
               <label>
-                <input type="checkbox"  className="checkbox"></input>I have read and agreed to the 
+                <input type="checkbox" className="checkbox"></input>I have read and agreed to the
                 <a href="/terms" className='checkboxLinks'>Terms of Service </a>and <a href="/privacy" className='checkboxLinks'>Privacy</a>
               </label>
+              {/* <div className="form-alert-box">
+                <p className="form-alert" style={{ display: `${usernameShow}` }}>This field is required. Please provide a valid username.</p>
+              </div> */}
+
               <div className="login-btn">
                 <button className="btnLogSign">Sign Up</button>
               </div>
